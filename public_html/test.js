b52db30a1c6d17fa7e570c6d894e281930f842c8
edtorbett <edtorbett@179126-dell.ssl.local>
2013-08-07 16:13:26 +0100

    Converted to use canvas instead of divs; some functionality lost at this stage Signed-off-by:edtorbett <edtorbett@179126-dell.ssl.local>

--- public_html/test.js
+++ public_html/test.js
@@ -1,232 +1,219 @@
-var playerDX = 0;
-var playerDY = 0;
+var canvas,
+        stage,	
+	screen_width,
+	screen_height,
+	player,
+	player_location = {},
+	player_direction = {},
+	player_speed = 2,
+	bullets = [],
+        counter = 0,
+        max_bullets = 1000,
+        bullet_speed = 3,
+        interval_between_bullets = 40,
+        bullet_lifespan = 1000;
+	
+var KEYCODE_SPACE = 32,
+	KEYCODE_UP = 38,
+	KEYCODE_DOWN = 40,
+	KEYCODE_LEFT = 37,
+	KEYCODE_RIGHT = 39,
+	KEYCODE_W = 87,
+	KEYCODE_S = 83,
+	KEYCODE_A = 65,
+	KEYCODE_D = 68; 
 
-var counter = 0;
-
-var maxBullets = 30;
-var intervalBetweenBullets = 5;
-
-var playerSpeed = 1.3;
-var bulletSpeed = 1.2;
-var bulletLifespan = 500;
-
-var inTarget = 0;
-
-var score = 0;
+var keys_pressed = {
+	KEYCODE_W:false,
+	KEYCODE_A:false,
+	KEYCODE_S:false,
+	KEYCODE_D:false,
+	KEYCODE_SPACE:false,
+	KEYCODE_UP:false,
+	KEYCODE_DOWN:false,
+	KEYCODE_LEFT:false,
+	KEYCODE_RIGHT:false,
+};
 
 function tick() {
-	var player = $("#player");
-	var target = $("#target");
-	var arena = $("#arena");
-	
-	var playerPos = player.position();
-	var targetPos = target.position();
-	
-	playerPos.top += playerDY;
-	playerPos.left += playerDX;
-	
-	if (playerPos.top < 16) {
-		playerPos.top = 16;
-	}
-	if (playerPos.left < 16) {
-		playerPos.left = 16;
-	}
-
-	if (playerPos.top > arena.height() - 16) {
-		playerPos.top = arena.height() - 16;
-	}
-	if (playerPos.left > arena.width() - 16) {
-		playerPos.left = arena.width() - 16;
-	}
-
-	player.css({"top": playerPos.top, "left": playerPos.left});
-	
-	var bullets = $(".bullet", arena);
-	
-	bullets.each(moveBullet);
+    
+    // update the stage		
+	if(player_direction.x != 0 || player_direction.y != 0){
+            player_location.x += (player_direction.x*player_speed);
+            player_location.y += (player_direction.y*player_speed);
+	}		
 	
 	counter += 1;	
 	
-	if (counter >= intervalBetweenBullets) {
+	if (counter >= interval_between_bullets) {
+            if (bullets.length >= max_bullets) {
+                bullets.shift().remove();
+            }
         
-                if (bullets.length >= maxBullets) {
-                    bullets.first().remove();
-                }
+            var startX = screen_width/2;
+            var startY = screen_height/2;
         
-		var angle = Math.atan2(playerPos.top - targetPos.top, playerPos.left - targetPos.left) * 180 / Math.PI;
+            var angle = Math.atan2(player_location.y - startY, player_location.x - startX);
 	
-		spawnBullet({
-			"top": targetPos.top,
-			"left": targetPos.left,
-			"speed": bulletSpeed,
-			"angle": angle,
-                        "life": bulletLifespan
-		});
+            var new_bullet = new bullet(startX,
+                    startY,
+                    angle,
+                    bullet_speed,
+                    bullet_lifespan);
+		bullets.push(new_bullet);		
+
 		counter = 0;
 	}
         
-        if (inTarget) {
-            score += 0.1;
-            if (score > 100) {
-                score = 100;
+        for(var index in bullets){			
+		bullets[index].tick();
+	}
+
+        stage.update();    
+}
+
+function bullet(x, y, angle, speed, life){
+	var self = this;
+	self.x = x;
+	self.y = y;
+	self.dx = Math.cos(angle) * speed;
+	self.dy = Math.sin(angle) * speed;
+        self.life = life;
+	self.db = new createjs.Shape();
+	self.tick = function(){
+            self.db.x = (self.x += self.dx) - (player_location.x - player.x);
+            self.db.y = (self.y += self.dy) - (player_location.y - player.y);
+            if ((--self.life) <= 0) {
+                self.remove();
             }
-        } else {
-            score -= 0.2;
-            if (score < 0) {
-                score = 0;
-            }
-        }
-        
-        $("#score").css("width", score + "%");
-        
-        centerArenaOnPlayer();
-}
-
-function centerArenaOnPlayer() {
-    var player = $("#player");
-    var arena = $("#arena");
-    var arenaBounds = $("#arenaBounds");
-
-    var playerPos = player.position();
-    var arenaCenterX = arenaBounds.width() / 2;
-    var arenaCenterY = arenaBounds.height() / 2;
-
-    arena.css({
-            "top": arenaCenterY - playerPos.top,
-            "left": arenaCenterX - playerPos.left
-        });
-}
-
-function moveBullet() {
-	var bullet = $(this);
-	
-	var arena = $("#arena");
-	var player = $("#player");
-	
-	var playerPos = player.position();
-	
-	var dX = bullet.data("dx");
-	var dY = bullet.data("dy");
-        var life = bullet.data("life") - 1;
-
-	var bulletPos = bullet.position();
-	
-	bulletPos.top += dY;
-	bulletPos.left += dX;
-	
-	if (bulletPos.top < 4) {
-		bullet.remove();
 	}
-	if (bulletPos.left < 4) {
-		bullet.remove();
+	self.remove = function(){
+		stage.removeChild(self.db);
 	}
-
-	if (bulletPos.top >= arena.height() - 4) {
-		bullet.remove();
-	}
-	if (bulletPos.left >= arena.width() - 4) {
-		bullet.remove();
-	}
-        
-	if (bulletPos.top > playerPos.top - 18
-            && bulletPos.top < playerPos.top + 18
-            && bulletPos.left > playerPos.left - 18
-            && bulletPos.left < playerPos.left + 18) {
-                score = 0;
-		bullet.remove();
-	}
-	if (bulletPos.left < 4) {
-		bullet.remove();
-	}
-
-	if (bulletPos.top >= arena.height() - 4) {
-		bullet.remove();
-	}
-	if (bulletPos.left >= arena.width() - 4) {
-		bullet.remove();
-	}   
-        
-        if (life <= 0) {
-            bullet.remove();
-        }
-	
-	bullet.css({"top": bulletPos.top, "left": bulletPos.left});
-        bullet.data("life", life);
-}
-
-function spawnBullet(bullet) {
-	
-	var dx = bullet.speed * Math.cos(bullet.angle * Math.PI / 180);
-	var dy = bullet.speed * Math.sin(bullet.angle * Math.PI / 180);
-	
-	var arena = $("#arena");
-	
-	$("<div/>")
-		.addClass("bullet")
-		.data({"dx": dx, "dy": dy, "life": bullet.life})
-		.css({"top": bullet.top, "left": bullet.left})
-		.appendTo(arena);
+	self.db.graphics.beginFill("red").drawCircle(0, 0, 5);	
+	self.db.x = x;
+	self.db.y = y;	
+	stage.addChild(self.db);
+	return self;
 }
 
 function keydown(event) {
-	var key = event.which;
-	if (key >= 37 && key <= 40) {
-		event.preventDefault();
-		switch (key) {
-			case 37: //Left
-				playerDX = -playerSpeed;
-				break;
-			case 38: //Up
-				playerDY = -playerSpeed;
-				break;
-			case 39: //Right
-				playerDX = playerSpeed;
-				break;
-			case 40: //Down
-				playerDY = playerSpeed;
-				break;
-		}
-	} else {
-		console.log("Down: " + key);
-	}
+    //cross browser issues exist
+    if (!e) { var e = window.event; }
+    switch (e.keyCode) {
+        case KEYCODE_W: 
+			keys_pressed.KEYCODE_W = true;
+            break;
+        case KEYCODE_UP: 
+			keys_pressed.KEYCODE_UP = true;
+            break;
+        case KEYCODE_S: 
+			keys_pressed.KEYCODE_S = true;
+            break;
+        case KEYCODE_DOWN: 
+			keys_pressed.KEYCODE_DOWN = true;
+			break;
+		case KEYCODE_A: 
+		keys_pressed.KEYCODE_A = true;
+            break;
+        case KEYCODE_LEFT: 
+			keys_pressed.KEYCODE_LEFT = true;
+            break;	
+        case KEYCODE_D: 
+			keys_pressed.KEYCODE_D = true;
+            break;
+        case KEYCODE_RIGHT: 
+			keys_pressed.KEYCODE_RIGHT = true;
+            break;
+    }
+    calculate_player_direction();    
 }
 
 function keyup(event) {
-	var key = event.which;
-	if (key >= 37 && key <= 40) {
-		event.preventDefault();
-		switch (key) {
-			case 37: //Left
-				playerDX = 0;
-				break;
-			case 38: //Up
-				playerDY = 0;
-				break;
-			case 39: //Right
-				playerDX = 0;
-				break;
-			case 40: //Down
-				playerDY = 0;
-				break;
-		}
+    //cross browser issues exist
+    if (!e) { var e = window.event; }
+    switch (e.keyCode) {	
+        case KEYCODE_W: 
+			keys_pressed.KEYCODE_W = false;
+			break;
+        case KEYCODE_UP: 
+			keys_pressed.KEYCODE_UP = false;
+			break;
+        case KEYCODE_S: 
+			keys_pressed.KEYCODE_S = false;
+			break;
+        case KEYCODE_DOWN: ;			
+			keys_pressed.KEYCODE_DOWN = false;
+			break;
+		case KEYCODE_A: 
+			keys_pressed.KEYCODE_A = false;
+			break;
+        case KEYCODE_LEFT: 
+			keys_pressed.KEYCODE_LEFT = false;
+			break;		
+        case KEYCODE_D: 
+			keys_pressed.KEYCODE_D = false;
+			break;
+        case KEYCODE_RIGHT: ; 
+			keys_pressed.KEYCODE_RIGHT = false;
+			break;
+    }		
+	calculate_player_direction();	
+}
+
+function calculate_player_direction(){
+	var up = keys_pressed.KEYCODE_W || keys_pressed.KEYCODE_UP,
+		down = keys_pressed.KEYCODE_S || keys_pressed.KEYCODE_DOWN,
+		left = keys_pressed.KEYCODE_A || keys_pressed.KEYCODE_LEFT,
+		right = keys_pressed.KEYCODE_D || keys_pressed.KEYCODE_RIGHT;
+	
+	if(up && down) {
+		player_direction.y = 0;
+	} else if(up && !down){
+		player_direction.y = -1;
+	} else if(!up && down){
+		player_direction.y = 1;
 	} else {
-		console.log("Up: " + key);
-	}	
-}
-
-function mouseovertarget() {
-    inTarget = 1;
-}
-
-function mouseleavetarget() {
-    inTarget = 0;
+		player_direction.y = 0;
+	}
+	
+	if(left && right) {
+		player_direction.x = 0;
+	} else if(left && !right){
+		player_direction.x = -1;
+	} else if(!left && right){
+		player_direction.x = 1;
+	} else {
+		player_direction.x = 0;
+	}
 }
 
 $(function() {
+	// Initialise keyboard controls
 	$(window).keydown(keydown);
 	$(window).keyup(keyup);
-
-	$("#target").hover(mouseovertarget, mouseleavetarget);
+        
+	//find canvas and load images, wait for last image to load
+        canvas = $("#canvas")[0];
+        // create a new stage and point it at our canvas:
+        stage = new createjs.Stage(canvas);
     
-	setInterval(tick, 25);
+	// Maximise the canvas + grab canvas width and height for later calculations
+	canvas.width = screen_width = 800;//window.innerWidth - 50;
+	canvas.height = screen_height = 600;//window.innerHeight - 50;
+		
+	player = new createjs.Shape();
+        player.graphics.beginFill("green").drawCircle(0, 0, 20);
+        player.x = screen_width/2;
+        player.y = screen_height/2;
+	player_location.x = player.x;
+	player_location.y = player.y;	
+	stage.addChild(player);
+	
+	player_direction.x = 0, player_direction.y = 0;
+		
+	createjs.Ticker.addListener(window);
+        // Best Framerate targeted (60 FPS)
+	createjs.Ticker.useRAF = true;
+	createjs.Ticker.setFPS(60);
 });
